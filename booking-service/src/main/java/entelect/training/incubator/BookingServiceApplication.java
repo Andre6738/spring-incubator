24c04ec84db28af9e5456a4d961469587cb1cd84
Andre Croukamp <andre.croukamp@entelect.co.za>
2023-03-14 11:23:13 +0200

    added new module

--- booking-service/src/main/java/entelect/training/incubator/BookingServiceApplication.java
+++ booking-service/src/main/java/entelect/training/incubator/BookingServiceApplication.java
@@ -1,7 +1,11 @@
 package entelect.training.incubator;
 
-public class Main {
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookingServiceApplication {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        SpringApplication.run(BookingServiceApplication.class, args);
     }
 }
