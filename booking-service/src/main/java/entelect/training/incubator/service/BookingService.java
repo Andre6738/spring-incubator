3f46701bad11c542cda086615b6c63f08932c1c2
Andre Croukamp <andre.croukamp@entelect.co.za>
2023-03-14 15:13:27 +0200

    booking works

---
+++ booking-service/src/main/java/entelect/training/incubator/service/BookingService.java
@@ -0,0 +1,50 @@
+package entelect.training.incubator.spring.customer.service;
+
+import entelect.training.incubator.spring.customer.model.Customer;
+import entelect.training.incubator.spring.customer.model.CustomerSearchRequest;
+import entelect.training.incubator.spring.customer.model.SearchType;
+import entelect.training.incubator.spring.customer.repository.CustomerRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.*;
+import java.util.function.Supplier;
+
+@Service
+public class CustomersService {
+
+    private final CustomerRepository customerRepository;
+
+    public CustomersService(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
+
+    public Customer createCustomer(Customer customer) {
+        return customerRepository.save(customer);
+    }
+
+    public List<Customer> getCustomers() {
+        Iterable<Customer> customerIterable = customerRepository.findAll();
+
+        List<Customer> result = new ArrayList<>();
+        customerIterable.forEach(result::add);
+
+        return result;
+    }
+
+    public Customer getCustomer(Integer id) {
+        Optional<Customer> customerOptional = customerRepository.findById(id);
+        return customerOptional.orElse(null);
+    }
+
+    public Customer searchCustomers(CustomerSearchRequest searchRequest) {
+        Map<SearchType, Supplier<Optional<Customer>>> searchStrategies = new HashMap<>();
+
+        searchStrategies.put(SearchType.NAME_SEARCH, () -> customerRepository.findByFirstNameAndLastName(searchRequest.getFirstName(), searchRequest.getLastName()));
+        searchStrategies.put(SearchType.PASSPORT_SEARCH, () -> customerRepository.findByPassportNumber(searchRequest.getPassport()));
+        searchStrategies.put(SearchType.USER_SEARCH, () -> customerRepository.findByUsername(searchRequest.getUsername()));
+
+        Optional<Customer> customerOptional = searchStrategies.get(searchRequest.getSearchType()).get();
+
+        return customerOptional.orElse(null);
+    }
+}
